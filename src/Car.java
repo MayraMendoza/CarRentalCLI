6cf952a59ce32f59f97d290c8fc0ca27a7988d94
MayraMendoza <mayra601m@gmail.com>
2022-01-24 17:13:45 -0500

    CarRentak CLI app- getting started

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+
+    public String make;
+    public String model;
+    public boolean isRented;
+
+
+    // constructor
+    public Car (String make, String model, boolean isRented){
+        this.make = make;
+        this.model = model;
+        this.isRented = isRented;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", isRented=" + isRented +
+                '}';
+    }
+}
