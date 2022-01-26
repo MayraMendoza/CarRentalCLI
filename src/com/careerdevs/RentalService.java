1715798bdb429b57527ed7b0a7ea7bdd65d1212e
MayraMendoza <mayra601m@gmail.com>
2022-01-25 19:14:34 -0500

    CarRental application version 1

--- src/com/careerdevs/RentalService.java
+++ src/com/careerdevs/RentalService.java
@@ -1,62 +1,57 @@
 package com.careerdevs;
 
+import com.careerdevs.ui.UserInput;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RentalService {
+
     public static void main(String[] args) {
+
+        Car[] carStorage = new Car[3];
         System.out.println("Welcome to the car Rental CLI");
         Car car1 = new Car("Honda", "Accord");
+        Car car2 = new Car("Chevy", "Cruze");
+        Car car3 = new Car("Toyota", "Corolla");
 
+        carStorage[0] = car1;
+        carStorage[1] = car2;
+        carStorage[2] = car3;
 
+//        System.out.println(availableCars[0].getMake());
+//
+////        availableCars[1].setRented(true);
+        int carAvailable = 3;
+        while(carAvailable> 0) {
 
-        Scanner scanner = new Scanner(System.in);
+            int availableCarNum = 0;
+            System.out.println("Available Cars: ");
+            for (int i = 0; i < carStorage.length; i++) {
 
+                if (carStorage[i].isRented() == false) {
+                    availableCarNum++;
+                    System.out.println("(" + availableCarNum + ") " + carStorage[i].getMake() + carStorage[i].getModel());
+                }
 
-//    String [] carsAvailable = {" Honda Accord", "Chevy Cruze", "Toyota Corolla"};
-        ArrayList<String> availableCars = new ArrayList<>();
-
-        availableCars.add("Honda Accord");
-        availableCars.add("Chevy Cruze");
-        availableCars.add("Toyota Corolla");
-
-        ArrayList<String> rentedCars = new ArrayList<>();
-        while (availableCars.size() != 0) {
-            System.out.println("available cars");
-            for (int i = 0; i < availableCars.size(); i++) {
-                System.out.println(i + 1 + ")" + availableCars.get(i));
             }
-
-            System.out.println("Enter a number to select the car you'd like to rent \nSelection: ");
-            int inputCarSelection = scanner.nextInt();
-            scanner.nextLine();
-            if (inputCarSelection == 1 || inputCarSelection == 2 || inputCarSelection == 3) {
-                rentedCars.add(availableCars.get(inputCarSelection - 1));
-                availableCars.remove(inputCarSelection - 1);
-            }
-
-//        while (inputCarSelection != 1 || inputCarSelection != 2 || inputCarSelection != 3 ){
-//            System.out.println("com.careerdevs.Car selection invalid, please try again");
-//            System.out.println("Enter a number to select the car you'd like to rent \nSelection: ");
-//            inputCarSelection = scanner.nextInt();
-//            scanner.nextLine();
-//        }
-////            if(inputCarSelection == 1 || inputCarSelection ==2 || inputCarSelection ==3 ){
-////                availableCars.remove(inputCarSelection-1);
-////                rentedCars.add(availableCars.get(inputCarSelection-1));
-////            }
-
-//            System.out.println("available cars");
-//            for (int i = 0; i < availableCars.size(); i++) {
-//                System.out.println(i + 1 + ")" + availableCars.get(i));
-//            }
-            System.out.println("rented cars");
-            for (int i = 0; i < rentedCars.size(); i++) {
-                System.out.println(i + 1 + ")" + rentedCars.get(i));
-            }
-
+            int userSelection = UserInput.ReadInt("Enter a number to select the car you'd like to rent \nSelection: ", 1, availableCarNum);
+            carStorage[userSelection - 1].setRented(true); //BUG (IF USER SELECTS 1 TWICE IT WILL SET INDEX 0 TO TRUE TWICE.
+            carAvailable--;
         }
         System.out.println("Sorry all cars have been rented");
+        int userInputEndProgram = UserInput.ReadInt("would you like to end this program or reset car data? (press 1 to end program or press 2 to reset data):",1,2);
+        if(userInputEndProgram ==2){
+            for(int i = 0; i < carStorage.length; i++){
+                carStorage[i].setRented(false);
+                carAvailable ++;
+
+            }
+        }else{
+            System.out.println("Have a nice day");
+        }
+
 
     }
+
 }
---
+++ src/com/careerdevs/ui/UserInput.java
@@ -0,0 +1,36 @@
+package com.careerdevs.ui;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInput {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String readString(String question) {
+        System.out.println(question);
+        return scanner.nextLine();
+    }
+
+    public static int ReadInt(String question, int Min, int Max) {
+        while (true) {
+            System.out.println(question);
+            try {
+                int answer = scanner.nextInt();
+                while(answer> Max || answer<Min){
+                    System.out.println("you must enter a valid number, please try again");
+                    System.out.println(question);
+                    answer=scanner.nextInt();
+                    scanner.nextLine();
+                }
+                return answer;
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("you must enter an integer, please try again");
+                System.out.println(question);
+
+            }
+
+        }
+    }
+}
